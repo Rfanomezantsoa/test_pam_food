5bd11a4a14af7653da3b55dd29bbe40171c0c85e
Rfanomezantsoa <78531350+Rfanomezantsoa@users.noreply.github.com>
2022-07-07 16:04:39 +0200

    delet all ignore

---
+++ storage/framework/views/7af00e507a43b2846abd5c0553b510b0f7c0922f.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    
+    <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
+
+
+</head>
+<body>
+    <div id="app"></div>
+
+    
+    <script type="text/javascript" src="<?php echo e(asset("js/app.js")); ?>"></script>
+</body>
+</html>
+<?php /**PATH D:\KANTY\CONCOURS\TEST_PAM\test_pam_food\resources\views/layouts/vue.blade.php ENDPATH**/ ?>
